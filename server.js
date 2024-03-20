cb63d11af091518727b14cb8845a9a3e51c08138
ameni <gharbiameni17@gmail.com>
2024-03-20 17:54:48 +0100

    changes

--- server.js
+++ server.js
@@ -1,7 +1,16 @@
 const express= require("express");
 const dotenv=require('dotenv');
+const morgan=require('morgan');
+const mongoose=require('mongoose');
 dotenv.config({path:"config.env"});
+mongoose.connect('mongodb://127.0.0.1:27017/nodeComm');
 const app=express();
+app.use(morgan('dev'));
+if(process.env.NODE_ENV==='development')
+{
+    app.use(morgan('dev'));
+    console.log(`mode: ${process.env.NODE_ENV}`)
+}
 app.get("/",(req,res)=>{
     res.send('our api v1');
 })
