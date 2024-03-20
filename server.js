2c58c1800f1309fcae7e8e0fbaeed64554505825
ameni <gharbiameni17@gmail.com>
2024-03-20 17:23:01 +0100

    changes

--- server.js
+++ server.js
@@ -1,9 +1,13 @@
 const express= require("express");
+const dotenv=require('dotenv');
+dotenv.config({path:"config.env"});
 const app=express();
 app.get("/",(req,res)=>{
-    res.send('our api');
+    res.send('our api v1');
 })
 
-app.listen(8001,()=>{
-    console.log("app run");
+const port=process.env.port||8000;
+
+app.listen(port,()=>{
+    console.log(`app run on port ${port}`);
 });
