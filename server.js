0c4f13c091943abd9d126a69b5377fd80628b3cc
ameni <gharbiameni17@gmail.com>
2024-03-25 01:51:33 +0100

    changes

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const dotenv=require('dotenv');
 const morgan=require('morgan');
 
 dotenv.config({path:"config.env"});
+const ApiError=require("./utils/apiError");
 const dbconn=require("./config/database");
 const categoryRoute=require('./routes/categRoute');
 dbconn();
@@ -15,7 +16,24 @@ if(process.env.NODE_ENV==='development')
     console.log(`mode: ${process.env.NODE_ENV}`)
 }
 
-app.use('/', categoryRoute);
+app.use('/categories', categoryRoute);
+
+app.all("*",(req,res,next)=>{
+ next(new ApiError(`can't find this route:${req.originalUrl}`,400))
+})
+
+//global error handling middleware 
+app.use((err,req,res,next)=>{
+    err.statusCode=err.statusCode||500;
+    err.status=err.status||"error";
+    
+    res.status(err.statusCode).json({
+        status:err.status,
+        error:err,
+        message:err.message,
+        stack:err.stack, 
+    });
+})
 const port=process.env.port||8000;
 
 app.listen(port,()=>{
